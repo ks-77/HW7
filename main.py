e330f51524f1729c4330c62ff163e6b67313ca11
ks-77 <deka0987654@gmail.com>
2024-01-23 23:57:02 +0200

    second task completed

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ number_inp = int(input("Enter a number: "))
 number_exp_inp = int(input("Enter a number exponent: "))
 result = get_number_pow(number_inp, number_exp_inp)
 print(f"\nyour number: '{number_inp}' raised to the power: '{number_exp_inp}' = {result}")
+
+# 2
+
+
+def stars(number_of_stars):
+    if number_of_stars <= 1:
+        return "*"
+
+    return number_of_stars * "*"
+
+
+print(stars(int(input("Enter a number of stars: "))))
