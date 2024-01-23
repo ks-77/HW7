3eb0b833f647ac4c62afed76ea4a21aeea8c502d
ks-77 <deka0987654@gmail.com>
2024-01-23 23:44:40 +0200

    first task completed

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
 HW7
 Savchenko Kirill
 """
+
+# 1
+
+
+def get_number_pow(number, number_pow):
+    if number_pow <= 1:
+        return number
+
+    return number * get_number_pow(number, number_pow - 1)
+
+
+number_inp = int(input("Enter a number: "))
+number_exp_inp = int(input("Enter a number exponent: "))
+result = get_number_pow(number_inp, number_exp_inp)
+print(f"\nyour number: '{number_inp}' raised to the power: '{number_exp_inp}' = {result}")
